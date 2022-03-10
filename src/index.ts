aaa241aa911fd1d6b3e76ff4cfff9c1461da901e
Sciator <39964450+Sciator@users.noreply.github.com>
2022-03-10 14:11:40 +0100

    feat: write query basics

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { InfluxDB } from "@influxdata/influxdb-client";
+import { InfluxDB, Point } from "@influxdata/influxdb-client";
 
 const { INFLUX_URL, INFLUX_TOKEN, INFLUX_ORG, INFLUX_BUCKET, } = process.env;
 
@@ -10,4 +10,15 @@ if (!INFLUX_URL || !INFLUX_TOKEN || !INFLUX_ORG || !INFLUX_BUCKET) {
 
 const db = new InfluxDB({ token: INFLUX_TOKEN, url: INFLUX_URL });
 
+const writeApi = db.getWriteApi(INFLUX_ORG, INFLUX_BUCKET, 'ns');
 
+const point1 = new Point('temperature')
+  .tag('sensor', 'example temp sensor tag')
+  .floatField('value', 20 + Math.round(100 * Math.random()) / 10)
+  .timestamp(new Date()) 
+
+console.log(`writing point: ${point1.toLineProtocol()}`);
+
+writeApi.writePoint(point1);
+
+writeApi.close();
